8d671059c72012aee96f9f2360789627a79ff700
Olga Chueshova <chueshova_olga@mail.ru>
2022-07-12 08:40:33 +0300

    the homework with if has been done

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const fam = prompt('Введите Вашу фамилию', 'Иванов');
 const name = prompt('Введите Ваше имя', 'Иван');
 const patr = prompt('Введите Ваше отчество', 'Иванович');
-const age = prompt('Сколько Вам лет?');
+let age = prompt('Сколько Вам лет?');
+
+let numAge = Number(age);
 
 let gender = confirm('Ваш пол мужской?');
-console.log(gender);
 
-if (gender.confirm =true) {
-    gender="мужской";}
+if (gender) {
+    gender = "мужской";
+}
 else {
-    gender="женский";
+    gender = "женский";
 }
 
-console.log(gender)
+if (numAge >= 65 && gender == 'мужской') {
+    pens = "да";
+}
+else if (numAge >= 55 && gender == 'женский') {
+    pens = "да";
+}
 
-alert('Ваше ФИО:');
+else {
+    pens = "нет";
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+let days = sum(numAge, 5);
+
+
+alert('Ваше ФИО: ' + fam + ' ' + name + ' ' + patr + ' \n' + 'Ваш возраст в годах: ' + numAge + ' \n' + 'Ваш возраст в днях: ' + numAge * 365 + '\n' + 'Через 5 лет Вам будет: ' + days + '\n' + 'Ваш пол: ' + gender + '\n' + 'Вы на пенсии: ' + pens);
